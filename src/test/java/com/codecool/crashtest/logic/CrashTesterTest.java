4bc5797250877f5d21e8ebfb7bdb89597458c12a
appuser <appuser@journey2-86bff4fbf7-5gdcq>
2023-07-27 12:02:34 +0000

    starter project

---
+++ src/test/java/com/codecool/crashtest/logic/CrashTesterTest.java
@@ -0,0 +1,8 @@
+package com.codecool.crashtest.logic;
+
+class CrashTesterTest {
+    int minimumSpeedToOpenTheAirbag = 30;
+    CrashTester crashTester = new CrashTester(minimumSpeedToOpenTheAirbag);
+
+
+}
